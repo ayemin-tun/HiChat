ced64a36c9cbfeb24b916aaa585da908917cfd04
AMTgasdfsa <amtamt547@gmail.com>
2024-03-17 15:40:53 +0630

    chat list is update when we send message

--- app/Livewire/Chat/ChatBox.php
+++ app/Livewire/Chat/ChatBox.php
@@ -8,34 +8,48 @@ use Livewire\Component;
 class ChatBox extends Component
 {
     public $selectedConversation;
+
     public $body;
+
     public $loadedMessages;
 
     public function mount()
     {
         $this->loadMessages();
     }
+
     public function sendMessage()
     {
         $this->validate([
-            'body' => 'required|string'
+            'body' => 'required|string',
         ]);
 
         $createdMessage = Message::create([
             'conversation_id' => $this->selectedConversation->id,
             'sender_id' => auth()->id(),
             'receiver_id' => $this->selectedConversation->getReceiver()->id,
-            'body' => $this->body
+            'body' => $this->body,
         ]);
         $this->reset(['body']);
+
         $this->dispatch('scroll-bottom');
+
         $this->loadedMessages->push($createdMessage);
+
+        //make conversition update list to top
+        $this->selectedConversation->updated_at = now();
+        $this->selectedConversation->save();
+
+        // refresh chatList
+        // $this->dispatchTo('chat.chat-list', 'refresh'); //livewire version 2
+        $this->dispatch('refresh')->to(ChatList::class);
     }
 
     public function loadMessages()
     {
         $this->loadedMessages = Message::where('conversation_id', $this->selectedConversation->id)->get();
     }
+
     public function render()
     {
         return view('livewire.chat.chat-box');
--- app/Livewire/Chat/ChatList.php
+++ app/Livewire/Chat/ChatList.php
@@ -2,17 +2,22 @@
 
 namespace App\Livewire\Chat;
 
+use Livewire\Attributes\On;
 use Livewire\Component;
 
 class ChatList extends Component
 {
     public $selectedConversation;
+
     public $query;
+
+    #[On('refresh')]
     public function render()
     {
         $user = auth()->user();
+
         return view('livewire.chat.chat-list', [
-            'conversations' => $user->conversations()->latest('updated_at')->get()
+            'conversations' => $user->conversations()->latest('updated_at')->get(),
         ]);
     }
 }
